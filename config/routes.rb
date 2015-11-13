29aaa0013f16fe9dc18a2cf9f823572729908ec9
Zach Schneider <zach@schneid.io>
2015-11-13 11:24:35 -0600

    homepage n template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'images#new'
 
+  get '/', to: 'images#home'
+  get '/pick', to: 'images#pick', as: 'pick'
   post '/', to: 'images#create'
-  resources :images, only: [:show]
+  get '/images/:id', to: 'images#show'
 end
