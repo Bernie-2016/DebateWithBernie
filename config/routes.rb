2e3c1b73a1ea300f3dc059f9bf30ca484907c6bd
Zach Schneider <zach@schneid.io>
2015-11-13 17:37:03 -0600

    Change path to make

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get '/', to: 'images#home'
-  get '/pick', to: 'images#pick', as: 'pick'
+  get '/make', to: 'images#make', as: 'make'
   post '/', to: 'images#create'
   resources :images, only: [:show] do
     get :download
