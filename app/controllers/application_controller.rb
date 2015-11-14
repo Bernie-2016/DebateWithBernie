1706e5ee92207e5c63c40b053e19b222db24cdce
Zach Schneider <zach@schneid.io>
2015-11-14 03:44:34 -0600

    Redirect if heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_if_heroku
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def redirect_if_heroku
+    redirect_to "http://debatewith.berniesanders.com#{request.fullpath}" if request.host == 'debate-with-bernie.herokuapp.com'
+  end
 end
