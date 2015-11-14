c2c2c650f2782e5729c7c607e1f5f7d2adfe8797
Zach Schneider <zach@schneid.io>
2015-11-14 03:39:23 -0600

    Set URL host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set URL host.
+  config.action_controller.default_url_options = { host: 'debatewith.berniesanders.com' }
 end
