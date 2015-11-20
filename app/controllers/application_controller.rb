ac47221e2aa7eceb138aaeba66f500adadba6f9f
Zach Schneider <zach@schneid.io>
2015-11-20 11:13:59 -0600

    Add https redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_filter :redirect_if_heroku
-
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :redirect_if_heroku!
 
   private
 
-  def redirect_if_heroku
-    redirect_to "http://debatewith.berniesanders.com#{request.fullpath}" if request.host == 'debate-with-bernie.herokuapp.com'
+  def redirect_if_heroku!
+    if request.host == 'debate-with-bernie.herokuapp.com' || request.protocol == 'http'
+      redirect_to "https://debatewith.berniesanders.com#{request.fullpath}"
+    end
   end
 end
