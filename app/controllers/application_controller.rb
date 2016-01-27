36d80f9dec4478bce6d85dfca0429a70c933e9a4
Zach Schneider <zach@schneid.io>
2016-01-27 15:45:32 -0600

    Fix ssl redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -70,6 +70,6 @@ class ApplicationController < ActionController::Base
   end
 
   def redirect_to_https!
-    redirect_to(protocol: 'https://') unless request.ssl? || request.local?
+    redirect_to(protocol: 'https://', host: domain_host) unless request.ssl? || request.local?
   end
 end
