d73ebc84d20d967775cf2dadcaeb88a40775714a
Zach Schneider <zach@schneid.io>
2016-01-27 16:09:37 -0600

    Serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -64,7 +64,7 @@ Rails.application.configure do
     else
       'https://debatewith.berniesanders.com'
     end
-end
+  end
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
